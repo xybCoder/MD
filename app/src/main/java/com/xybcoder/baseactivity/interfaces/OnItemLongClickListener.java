b86b5d3efe5871dd03fe0132318b8b45443be529
xybCoder <xyb520zly@gmail.com>
2016-05-20 21:18:40 +0800

    add baseAdapter

---
+++ app/src/main/java/com/xybcoder/baseactivity/interfaces/OnItemLongClickListener.java
@@ -0,0 +1,10 @@
+package com.xybcoder.baseactivity.interfaces;
+
+import android.view.View;
+
+/**
+ * Created by zzy on 2015/7/29.
+ */
+public interface OnItemLongClickListener {
+    void onLongClick(View view, int position);
+}

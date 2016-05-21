4d7384a51e2c445ef9685b1a70d91ad26494c946
xybCoder <xyb520zly@gmail.com>
2016-05-22 00:05:54 +0800

    add utils tool

--- common/src/main/java/com/library/common/util/DigestUtil.java
+++ common/src/main/java/com/library/common/util/DigestUtil.java
@@ -1,6 +1,9 @@
 package com.library.common.util;
 
+import android.util.Base64;
+
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by dell on 2016/5/20.
@@ -34,6 +37,51 @@ public class DigestUtil {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * 对数据（字节）进行Base64编码
+     *
+     * @param data 要编码的数据（字节数组）
+     * @return 返回编码后的字符串
+     */
+    public static String Base64Encode(byte[] data) {
+        String ret = null;
+        if (data != null && data.length > 0) {
+            ret = Base64.encodeToString(data, Base64.NO_WRAP);
+        }
+        return ret;
+    }
+
+    /**
+     * 对Base64编码后的数据进行还原
+     *
+     * @param data 使用Base64编码过的数据
+     * @return 返回还原后的数据（字节数组）
+     */
+    public static byte[] Base64Decode(String data) {
+        byte[] ret = null;
+        if (data != null && data.length() > 0) {
+            ret = Base64.decode(data, Base64.NO_WRAP);
+        }
+        return ret;
+    }
+
+    /**
+     * 使用MD5获取数据的摘要信息
+     *
+     * @param data 数据
+     * @return 摘要信息
+     */
+    public static String toMD5(byte[] data) {
+        String ret = null;
+        try {
+            byte[] digest = MessageDigest.getInstance("md5").digest(data);
+            ret = Base64Encode(digest);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return ret;
     }
 
     /**
--- common/src/main/java/com/library/common/util/ScreenUtil.java
+++ common/src/main/java/com/library/common/util/ScreenUtil.java
@@ -1,9 +1,10 @@
 package com.library.common.util;
 
 import android.content.Context;
+import android.util.TypedValue;
 
 /**
- * ScreenUtils
+ * 常用单位转换的辅助类，涉及到单位转换的方法都在这里
  * <ul>
  * <strong>Convert between dp and sp</strong>
  * <li>{@link ScreenUtils#dpToPx(Context, float)}</li>
@@ -17,6 +18,9 @@ public class ScreenUtil {
         throw new AssertionError();
     }
 
+    /**
+     * dp转px
+     */
     public static float dpToPx(Context context, float dp) {
         if (context == null) {
             return -1;
@@ -24,6 +28,9 @@ public class ScreenUtil {
         return dp * context.getResources().getDisplayMetrics().density;
     }
 
+    /**
+     * px转dp
+     */
     public static float pxToDp(Context context, float px) {
         if (context == null) {
             return -1;
@@ -31,10 +38,43 @@ public class ScreenUtil {
         return px / context.getResources().getDisplayMetrics().density;
     }
 
+    /**
+     * sp转px
+     */
+    public static int spToPx(Context context, float spVal) {
+        if (context == null) {
+            return -1;
+        }
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,
+                spVal, context.getResources().getDisplayMetrics());
+    }
+
+    /**
+     * px转sp
+     */
+    public static float pxToSp(Context context, float pxVal) {
+        if (context == null) {
+            return -1;
+        }
+        return (pxVal / context.getResources().getDisplayMetrics().scaledDensity);
+    }
+
+    /**
+     *
+     * @param context
+     * @param dp
+     * @return
+     */
     public static int dpToPxInt(Context context, float dp) {
         return (int)(dpToPx(context, dp) + 0.5f);
     }
 
+    /**
+     *
+     * @param context
+     * @param px
+     * @return
+     */
     public static int pxToDpCeilInt(Context context, float px) {
         return (int)(pxToDp(context, px) + 0.5f);
     }
